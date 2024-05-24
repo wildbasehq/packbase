4f68f1e1a24f0bd252d36efe68e7d96b92e91b44
Rek <im@rek.lol>
2024-05-24 14:42:02 +1000

    🐛 Resolve vercel build failure related to env

--- next.config.js
+++ next.config.js
@@ -8,11 +8,11 @@ const nextConfig = {
         domains: ['ecwxmakixjrsklydfmtl.supabase.co', 'cdn.discordapp.com', 'unavatar.io', 'localhost'],
     },
     env: {
-        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
 
-        NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
-        NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        NEXT_PUBLIC_POSTHOG_KEY: process.env.POSTHOG_KEY,
+        NEXT_PUBLIC_POSTHOG_HOST: process.env.POSTHOG_HOST,
     },
     webpack: (config) => {
         return config;
