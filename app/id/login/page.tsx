61ce4e1da6612fae1629cfa7bc9ec4565d58ac18
rekkisomo <rekh@skiff.com>
2024-05-27 23:51:06 +1000

    🚸 "You'll need an invite code" call out on sign up

--- app/id/login/page.tsx
+++ app/id/login/page.tsx
@@ -6,6 +6,8 @@ import {Heading, Text} from '@/components/shared/text'
 import {ProjectName} from '@/lib/utils'
 import {Input} from '@/components/shared/input/text'
 import LoginGradient from '@/app/id/login/client/gradient'
+import {Alert, AlertDescription, AlertTitle} from '@/components/shared/ui/alert'
+import {MailQuestion} from 'lucide-react'
 
 export default function IDLogin({searchParams}: {
     searchParams: { error_description: string; error: string; };
@@ -19,11 +21,11 @@ export default function IDLogin({searchParams}: {
     return (
         <>
             {/* test */}
-            <div className="animate-slide-down-fade absolute inset-0 bg-surface-container-low -z-10 dark:opacity-75" style={{
+            <div className="animate-slide-down-fade absolute inset-0 -z-10 dark:opacity-75" style={{
                 // fit to screen
                 background: 'url(/img/illustrations/home/fox-in-snowy-oasis.webp) center/cover no-repeat',
             }}>
-                <LoginGradient />
+                <LoginGradient/>
             </div>
 
             <div
@@ -42,7 +44,7 @@ export default function IDLogin({searchParams}: {
                         display_name: 'Rek ✨',
                         username: 'rek',
                         avatar: '/img/illustrations/onboarding/pfp/rekkisomo.png'
-                    }} />
+                    }}/>
                 </div>
                 <div
                     className="relative animate-slide-down-fade rounded bg-card-solid border shadow h-fit sm:max-w-md top-0 mx-auto">
@@ -66,29 +68,41 @@ export default function IDLogin({searchParams}: {
                                             Create an account
                                         </h1>
                                     </div>
+
+                                    <Alert>
+                                        <AlertTitle>
+                                            <MailQuestion className="inline-flex h-5 w-5"/> You'll need an invite code
+                                        </AlertTitle>
+                                        <AlertDescription>
+                                            <p>It's required in the next step. Don't have one? Generous users who have
+                                                access can see a list of
+                                                users without an invite (like you) and gift one to them!~</p>
+                                            <p className="text-tertiary">If you've traded anything for an invite,
+                                                you've been scammed.</p>
+                                        </AlertDescription>
+                                    </Alert>
+
                                     <form className="space-y-1">
                                         <div>
-                                            <Input id="register-email" label="E-Mail" type="email" />
+                                            <Input id="register-email" label="E-Mail" type="email"/>
                                         </div>
                                         <div>
-                                            <Input id="register-pass" label="Password" type="password" />
+                                            <Input id="register-pass" label="Password" type="password"/>
                                         </div>
                                         <div>
-                                            <Input id="register-pass-conf" label="Confirm Password" type="password" />
+                                            <Input id="register-pass-conf" label="Confirm Password" type="password"/>
                                         </div>
-                                        <Button>
-                                            Register
-                                        </Button>
                                     </form>
+
                                     <p className="px-8 text-center text-sm text-muted-foreground">
-                                        By clicking continue, you agree to our{" "}
+                                        By clicking continue, you agree to our{' '}
                                         <Link
                                             href="/terms"
                                             className="underline underline-offset-4 hover:text-primary"
                                         >
                                             Terms of Service
-                                        </Link>{" "}
-                                        and{" "}
+                                        </Link>{' '}
+                                        and{' '}
                                         <Link
                                             href="/privacy"
                                             className="underline underline-offset-4 hover:text-primary"
