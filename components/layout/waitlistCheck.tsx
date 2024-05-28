c8e553545f1caa208abd5c68f8a93ab782b70895
rekkisomo <rekh@skiff.com>
2024-05-28 11:26:58 +1000

    🚸 Hide waitlist banner on guest

--- components/layout/waitlistCheck.tsx
+++ components/layout/waitlistCheck.tsx
@@ -4,39 +4,39 @@ import {useEffect, useState} from 'react'
 import {HandHeartIcon, ShieldAlertIcon} from 'lucide-react'
 import {LoadingCircle} from '@/components/shared/icons'
 
+const ServiceStates = {
+    free: {
+        icon: HandHeartIcon,
+        status: 'Welcome!',
+        text: 'If you\'re reading this, congrats~! Someone gifted you an invite code!'
+    },
+    ban: {
+        icon: ShieldAlertIcon,
+        status: 'Invite Revoked',
+        text: 'Your invite was revoked as you violated our guidelines.'
+    },
+    wait: {
+        icon: LoadingCircle,
+        status: 'Waiting',
+        text: 'You\'re on the waitlist. You\'ll need an invite code from someone, or wait for us to open.'
+    },
+    dummy: {
+        icon: LoadingCircle,
+        status: 'Checking...',
+        text: 'Checking...'
+    }
+}
+
 export default function WaitlistCheck() {
     const {user} = useUserAccountStore()
     const [serviceStatus, setServiceStatus] = useState<'dummy' | 'free' | 'ban' | 'wait'>('dummy')
 
-    const ServiceStates = {
-        free: {
-            icon: HandHeartIcon,
-            status: 'Welcome!',
-            text: 'If you\'re reading this, congrats~! Someone gifted you an invite code!'
-        },
-        ban: {
-            icon: ShieldAlertIcon,
-            status: 'Invite Revoked',
-            text: 'Your invite was revoked as you violated our guidelines.'
-        },
-        wait: {
-            icon: LoadingCircle,
-            status: 'Waiting',
-            text: 'You\'re on the waitlist. You\'ll need an invite code from someone, or wait for us to open.'
-        },
-        dummy: {
-            icon: LoadingCircle,
-            status: 'Checking...',
-            text: 'Checking...'
-        }
-    }
-
     useEffect(() => {
         setServiceStatus(user?.waitlistType || 'wait')
-    })
+    }, [])
 
     const CurrentServiceIcon = ServiceStates[serviceStatus].icon
-
+    if (!user) return
     return (
         <div
             className={`relative shadow-inner flex flex-col items-start justify-between gap-x-8 gap-y-4 border-b bg-sidebar px-4 py-4 sm:flex-row sm:items-center sm:px-6 lg:px-8 ${serviceStatus === 'dummy' && 'before:animate-[shimmer_1s_linear_infinite] shimmer-template'}`}>
