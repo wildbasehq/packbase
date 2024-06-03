4991daae8be3e6c7507495f4cc928ca57b1df456
rekkisomo <rekh@skiff.com>
2024-06-03 23:57:30 +1000

    🐛 Settings should require authentication

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -3,12 +3,16 @@ import {useEffect} from 'react'
 import Body from '@/components/layout/body'
 import {useResourceUIStore, useUserAccountStore} from '@/lib/states'
 import {Cog6ToothIcon} from '@heroicons/react/24/solid'
+import {useRouter} from 'next/navigation'
 
 export default function Settings({children}: {
     children: React.ReactNode
 }) {
     const {user} = useUserAccountStore()
+    const router = useRouter()
     const {setNavigation} = useResourceUIStore()
+
+    if (!user) return router.push('/id/login')
     useEffect(() => {
         setNavigation([{
             name: 'General',
