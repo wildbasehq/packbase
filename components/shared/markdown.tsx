1b02af26414c9a135f51b315a99cb915e08d7770
rekkisomo <rekh@skiff.com>
2024-06-12 12:19:20 +1000

    🐛 Heading 1-3 correct sizing

--- components/shared/markdown.tsx
+++ components/shared/markdown.tsx
@@ -1,20 +1,27 @@
-import {Heading, Text} from '@/components/shared/text'
-import ReactMarkdown from 'react-markdown'
+import { Heading, Text } from "@/components/shared/text";
+import ReactMarkdown from "react-markdown";
 
-export default function Markdown({children, ...props}: {
-    children: string
-    [x: string]: any
-}) {
+export default function Markdown({ children, ...props }: { children: string; [x: string]: any }) {
     return (
-        <ReactMarkdown className="text-sm" components={{
-            h1(props) {
-                return <Heading {...props}/>
-            },
-            p(props) {
-                return <Text {...props}/>
-            }
-        }} {...props}>
+        <ReactMarkdown
+            className="text-sm"
+            components={{
+                h1(props) {
+                    return <Heading as="h1" size="3xl" {...props} />;
+                },
+                h2(props) {
+                    return <Heading as="h2" size="2xl" {...props} />;
+                },
+                h3(props) {
+                    return <Heading as="h3" size="xl" {...props} />;
+                },
+                p(props) {
+                    return <Text {...props} />;
+                },
+            }}
+            {...props}
+        >
             {children}
         </ReactMarkdown>
-    )
+    );
 }
