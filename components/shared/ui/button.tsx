fd55175f1534711c040c4c97db22ee1d7a02f467
rekkisomo <rekh@skiff.com>
2024-09-22 15:17:05 +1000

    💄 Button rounding should follow UI

--- components/shared/ui/button.tsx
+++ components/shared/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils/cn'
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center whitespace-nowrap select-none rounded-md text-default text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:ring-4 hover:ring-n-5/10 disabled:cursor-not-allowed disabled:opacity-50',
+    'inline-flex items-center justify-center whitespace-nowrap select-none rounded text-default text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 hover:ring-4 hover:ring-n-5/10 disabled:cursor-not-allowed disabled:opacity-50',
     {
         variants: {
             variant: {
