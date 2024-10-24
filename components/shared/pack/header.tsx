5d63dca813e6366d32cb93070a1e06476feb59f7
rekkisomo <rekh@skiff.com>
2024-10-24 17:17:19 +1100

    ✨ Pack header

---
+++ components/shared/pack/header.tsx
@@ -0,0 +1,46 @@
+import { Heading } from '@/components/shared/text'
+import Markdown from '@/components/shared/markdown'
+
+export default function PackHeader({ ...props }: any) {
+    const pack = props.pack
+
+    return (
+        <div className="relative">
+            <div>
+                <img
+                    className="pointer-events-none aspect-banner w-full rounded-bl rounded-br object-cover"
+                    src={pack.images?.header || '/img/background/generic-generated.png'}
+                    alt="Profile cover"
+                />
+            </div>
+            <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+                <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+                    <div className="flex">
+                        <img
+                            className="bg-default ring-default pointer-events-none h-24 w-24 rounded-lg ring-1 sm:h-32 sm:w-32"
+                            src={pack.images?.avatar || '/img/default-avatar.png'}
+                            alt=""
+                        />
+                    </div>
+                    <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                        <div className="mt-6 min-w-0 flex-1 sm:hidden md:block">
+                            <Heading>{pack.display_name || pack.slug}</Heading>
+                            {/* Small @username */}
+                            <div className="flex items-center">
+                                <p className="text-alt-2 truncate text-sm font-medium">@{pack.slug}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-6 hidden min-w-0 flex-1 sm:block md:hidden">
+                    <Heading>{pack.display_name || pack.slug}</Heading>
+                </div>
+                <div className="text-default block min-w-0 flex-1">
+                    <div className="mt-6 whitespace-pre-line text-sm">
+                        <Markdown>{pack.about?.bio}</Markdown>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
