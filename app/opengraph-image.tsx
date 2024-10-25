64bd5138478380fcd790fdca47576540ba7cf65b
rekkisomo <rekh@skiff.com>
2024-10-25 20:12:04 +1100

    📝 Remove all mention of Yipnyap

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,40 +1,44 @@
 /* eslint-disable @next/next/no-img-element */
-import {ImageResponse} from 'next/server'
+import { ImageResponse } from 'next/server'
 import React from 'react'
 
 export const runtime = 'edge'
-export const alt = 'Yipnyap is a place where furries can be themselves.'
+export const alt = 'Packbase is a maze not meant for you.'
 export const contentType = 'image/png'
 
 export default async function OG() {
     return new ImageResponse(
         (
-            <div tw="flex flex-col p-16"
-                 style={{
-                     height: '100%',
-                     width: '100%',
-                     display: 'flex',
-                     fontFamily: 'Lexend',
-                     backgroundImage: 'linear-gradient(to bottom right, #E0E7FF 25%, #ffffff 50%, #CFFAFE 75%)',
-                 }}
+            <div
+                tw="flex flex-col p-16"
+                style={{
+                    height: '100%',
+                    width: '100%',
+                    display: 'flex',
+                    fontFamily: 'Lexend',
+                    backgroundImage: 'linear-gradient(to bottom right, #E0E7FF 25%, #ffffff 50%, #CFFAFE 75%)',
+                }}
             >
                 <div tw="flex flex-row items-center mb-12">
                     {/* @ts-ignore */}
                     <svg viewBox="0 0 27 22" aria-hidden="true" tw="w-20 h-16 opacity-95 mr-4">
                         <linearGradient id="ogLogoGrad" x1="0%" y1="0%" x2="100%" y2="100%">
-                            <stop offset="0%" stopColor="#60a5fa"/>
-                            <stop offset="100%" stopColor="#3b82f6"/>
+                            <stop offset="0%" stopColor="#60a5fa" />
+                            <stop offset="100%" stopColor="#3b82f6" />
                         </linearGradient>
-                        <path fill="url(#ogLogoGrad)"
-                              d="M19.72,1.99c.05-.12,0-.2-.14-.16l-3.99,1.04c-.13,.04-.31,.15-.4,.25l-3.01,3.42c-.09,.1-.05,.2,.08,.23l4.77,.78c.13,.02,.29-.06,.34-.19l2.34-5.37Zm7.1,10.39c.13,.02,.21,.14,.17,.27l-.5,1.61c-.04,.13-.16,.3-.28,.37l-1.9,1.29c-.11,.08-.32,.15-.45,.16l-8.15,.7c-.14,.01-.32,.1-.41,.2l-4.57,4.97c-.09,.1-.21,.08-.26-.05l-1.75-4.29c-.05-.13-.2-.22-.34-.21l-6.62,.57c-.14,.01-.22-.09-.18-.22l1.46-5.13c.04-.13-.03-.28-.16-.34L.1,11.05c-.12-.06-.13-.16-.02-.24l4.18-2.96c.11-.08,.2-.14,.2-.14,0,0,.05-.1,.12-.22L7.28,2.63c.07-.12,.22-.27,.34-.33L12.12,.02c.12-.06,.2,0,.17,.13l-1.41,6.99c-.03,.13,.06,.26,.2,.28l7.43,1.2c.13,.02,.3,.13,.37,.25l1.5,2.4c.07,.12,.24,.22,.37,.25l6.05,.86Z"/>
-                        <path fill="#FFF"
-                              d="M14.45,9.91c-.09-.1-.05-.17,.08-.15l3.16,.45c.13,.02,.3,.13,.37,.25l.25,.4c.07,.12,.04,.27-.07,.35l-.63,.43c-.11,.08-.32,.15-.45,.16l-.77,.06c-.14,.01-.32-.06-.41-.17l-1.53-1.78Z"/>
+                        <path
+                            fill="url(#ogLogoGrad)"
+                            d="M19.72,1.99c.05-.12,0-.2-.14-.16l-3.99,1.04c-.13,.04-.31,.15-.4,.25l-3.01,3.42c-.09,.1-.05,.2,.08,.23l4.77,.78c.13,.02,.29-.06,.34-.19l2.34-5.37Zm7.1,10.39c.13,.02,.21,.14,.17,.27l-.5,1.61c-.04,.13-.16,.3-.28,.37l-1.9,1.29c-.11,.08-.32,.15-.45,.16l-8.15,.7c-.14,.01-.32,.1-.41,.2l-4.57,4.97c-.09,.1-.21,.08-.26-.05l-1.75-4.29c-.05-.13-.2-.22-.34-.21l-6.62,.57c-.14,.01-.22-.09-.18-.22l1.46-5.13c.04-.13-.03-.28-.16-.34L.1,11.05c-.12-.06-.13-.16-.02-.24l4.18-2.96c.11-.08,.2-.14,.2-.14,0,0,.05-.1,.12-.22L7.28,2.63c.07-.12,.22-.27,.34-.33L12.12,.02c.12-.06,.2,0,.17,.13l-1.41,6.99c-.03,.13,.06,.26,.2,.28l7.43,1.2c.13,.02,.3,.13,.37,.25l1.5,2.4c.07,.12,.24,.22,.37,.25l6.05,.86Z"
+                        />
+                        <path
+                            fill="#FFF"
+                            d="M14.45,9.91c-.09-.1-.05-.17,.08-.15l3.16,.45c.13,.02,.3,.13,.37,.25l.25,.4c.07,.12,.04,.27-.07,.35l-.63,.43c-.11,.08-.32,.15-.45,.16l-.77,.06c-.14,.01-.32-.06-.41-.17l-1.53-1.78Z"
+                        />
                     </svg>
-                    <h1 tw="text-3xl font-bold text-black">Yipnyap</h1>
+                    <h1 tw="text-3xl font-bold text-black">Packbase</h1>
                     {/* Staging pill */}
                     <div tw="ml-2 flex">
-                        <span
-                            tw="items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
+                        <span tw="items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
                             ✨ Staging {process.env.NEXT_PUBLIC_SHA || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || 'unknown'}
                         </span>
                     </div>
