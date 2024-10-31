9a51535bf6da2b0b3c73f2e04d2bd6a0b1a007c4
rekkisomo <rekh@skiff.com>
2024-11-01 02:10:34 +1100

    🔉 Toaster notif sound from Yipnyap

--- lib/toast.tsx
+++ lib/toast.tsx
@@ -1,11 +1,55 @@
 import { ExternalToast, toast as SonnerToast } from 'sonner'
 
+const whichSound = localStorage.getItem('yipnyap_sound') || 'donk'
+const filesMP3 = ['lizzo', 'voice_sans', 'WAAAAAAA']
+const fileType = filesMP3.includes(whichSound) ? 'mp3' : 'ogg'
+const audio = new Audio(`/audio/${whichSound}.${fileType}`)
+
+const warnPlay = (audio: HTMLAudioElement) => {
+    const audio2 = new Audio(`/audio/${whichSound}.ogg`)
+
+    setTimeout(() => {
+        audio2.play().then(() => {
+            audio2.remove()
+        })
+    }, 150)
+}
+
+const errorPlay = (audio: HTMLAudioElement) => {
+    if (audio.src.includes('.mp3')) return
+    const audio2 = new Audio(`/audio/${whichSound}.ogg`)
+
+    setTimeout(() => {
+        audio2.preservesPitch = false
+        audio2.playbackRate = 1.2
+        audio2.play().then(() => {
+            audio2.remove()
+
+            setTimeout(() => {
+                const audio3 = new Audio(`/audio/${whichSound}.ogg`)
+                audio3.preservesPitch = false
+                audio3.playbackRate = 0.8
+                audio3.play().then(() => {
+                    audio3.remove()
+                })
+            }, 150)
+        })
+    }, 150)
+}
 export const toast = {
     success(message: string | React.ReactNode, data?: ExternalToast) {
+        audio.play().then(() => {
+            audio.remove()
+        })
+
         return SonnerToast.success(arguments)
     },
 
     error(message: string | React.ReactNode, data?: ExternalToast) {
+        audio.play().then(() => {
+            audio.remove()
+        })
+        errorPlay(audio)
         return SonnerToast.success(arguments)
     },
 }
