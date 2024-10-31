b2958f65914e8e6a134be4f2b3b845201e04b7c3
rekkisomo <rekh@skiff.com>
2024-11-01 02:12:05 +1100

    🐛 WHAT THE FUCKJ

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -1,104 +1,104 @@
-"use client";
+'use client'
 // export const API_URL = (typeof window !== 'undefined' ? (
 //     window.location.hostname === 'localhost'
 //         ? 'http://localhost/api/'
 //         : `${window.location.protocol}//api.${window.location.hostname.replace('www.', '')}/api/`) : '/api/') + 'v2/';
-import { ProjectDeps, ProjectName } from "@/lib/utils";
-import { createClient } from "@/lib/supabase/client";
+import { ProjectDeps, ProjectName } from '@/lib/utils'
+import { createClient } from '@/lib/supabase/client'
 
-export const API_URL = process.env.NEXT_PUBLIC_YAPOCK_URL;
-let TOKEN: string | undefined;
+export const API_URL = process.env.NEXT_PUBLIC_YAPOCK_URL
+let TOKEN: string | undefined
 
 const FETCH_BASE = async (url: string, options: RequestInit = {}) => {
-    const supabase = createClient();
-    TOKEN = (await supabase.auth.getSession()).data.session?.access_token;
+    const supabase = createClient()
+    TOKEN = (await supabase.auth.getSession()).data.session?.access_token
     const headers = new Headers({
-        "Content-Type": "application/json",
-        Accept: "application/json",
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
         ...(TOKEN
             ? {
                   Authorization: `Bearer ${TOKEN}`,
               }
             : {}),
-        "X-Wolfbite-Project-Id": ProjectName,
-        "X-Wolfbite-Project-Deps": ProjectDeps.join(","),
-    });
+        'X-Wolfbite-Project-Id': ProjectName,
+        'X-Wolfbite-Project-Deps': ProjectDeps.join(','),
+    })
 
     const defaultOptions = {
         headers,
         ...options,
-    };
+    }
 
-    const response = await fetch(url, defaultOptions);
-    let data;
+    const response = await fetch(url, defaultOptions)
+    let data
     try {
-        data = await response.json();
+        data = await response.json()
     } catch (_) {
         // cry about it, nextjs. seriously.
     }
     switch (response.status) {
         case 401:
             throw new Error(
-                data?.message || "UNAUTHORIZED",
+                data?.message || 'UNAUTHORIZED',
                 data?.name && {
                     cause: data.name,
                 },
-            );
+            )
         case 403:
             throw new Error(
-                data?.message || "FORBIDDEN",
+                data?.message || 'FORBIDDEN',
                 data?.name && {
                     cause: data.name,
                 },
-            );
-        // case 404:
-        // throw new Error(data.message || 'NOT_FOUND')
+            )
+        case 404:
+            throw new Error(data.message || 'NOT_FOUND')
         case 500:
             throw new Error(
-                data?.message || "INTERNAL_SERVER_ERROR",
+                data?.message || 'INTERNAL_SERVER_ERROR',
                 data?.name && {
                     cause: data.name,
                 },
-            );
+            )
         default:
-            break;
+            break
     }
 
     return {
         data,
         status: response.status,
-    };
-};
+    }
+}
 
 export const FetchHandler = {
     API_URL,
     TOKEN,
     get: async (name: string, options: RequestInit = {}) => {
-        const url = `${API_URL}${name}`;
-        return await FETCH_BASE(url, options);
+        const url = `${API_URL}${name}`
+        return await FETCH_BASE(url, options)
     },
 
     post: (name: string, options: RequestInit = {}) => {
-        const url = `${API_URL}${name}/`;
+        const url = `${API_URL}${name}/`
         return FETCH_BASE(url, {
             ...options,
-            method: "POST",
-        });
+            method: 'POST',
+        })
     },
 
     put: (name: string, options: RequestInit = {}) => {
-        const url = `${API_URL}${name}/`;
+        const url = `${API_URL}${name}/`
         return FETCH_BASE(url, {
             ...options,
-            method: "PUT",
-        });
+            method: 'PUT',
+        })
     },
 
     delete: (name: string, options: RequestInit = {}) => {
-        const url = `${API_URL}${name}/`;
+        const url = `${API_URL}${name}/`
         return FETCH_BASE(url, {
             ...options,
-            method: "DELETE",
-        });
+            method: 'DELETE',
+        })
     },
-};
+}
