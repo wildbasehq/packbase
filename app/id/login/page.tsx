947c141b23670768d56333a728d2f1698da1971f
Rekkisomo Harley <rekkisomo@pm.me>
2024-11-12 14:20:36 +1100

    fix: button button styling on light mode

--- app/id/login/page.tsx
+++ app/id/login/page.tsx
@@ -108,7 +108,7 @@ export default function IDLogin({
                             variant="default"
                             type="submit"
                             disabled={submitting}
-                            className="flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            className="flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         >
                             {!submitting ? 'Sign in' : <LoadingCircle />}
                         </Button>
--- components/shared/ui/button.tsx
+++ components/shared/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
     {
         variants: {
             variant: {
-                default: 'bg-card text-default ring-default border !no-underline transition-all hover:bg-n-2/25 hover:ring-4 dark:hover:bg-n-6/50',
+                default: 'bg-card ring-default border !no-underline transition-all hover:bg-n-2/25 hover:ring-4 dark:hover:bg-n-6/50',
                 primary: 'bg-primary text-white hover:bg-primary/90',
                 destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/75 hover:ring-destructive/25',
                 outline: 'ring-default border !no-underline transition-all hover:bg-n-2/25 hover:ring-4 dark:hover:bg-n-6/50',
