e468a1409da17ba6c6441b4bc593dbd40a836d1e
Rek <rekkisomo@pm.me>
2025-02-05 20:25:28 +1100

    fix: Heatbeat danger text uncoloured

--- components/layout/resource-switcher/pack-switcher.tsx
+++ components/layout/resource-switcher/pack-switcher.tsx
@@ -61,7 +61,7 @@ export default function PackSwitcher() {
                             </TooltipContent>
                             <div className="mt-1 grid grid-rows-1 divide-y border-t">
                                 <div className="grid grid-cols-2 gap-2 divide-x px-2 [&>*:not(:first-child)]:pl-2 [&>*]:py-1">
-                                    <Text className={(item.statistics?.heartbeat || 0) < item.statistics?.members ? 'text-tertiary' : 'text-alt'}>
+                                    <Text className={(item.statistics?.heartbeat || 0) < item.statistics?.members ? '!text-tertiary' : 'text-alt'}>
                                         <ECGIcon className="-mt-0.5 inline-flex h-4 w-4" /> {item.statistics?.heartbeat || 0}
                                     </Text>
                                     <Text alt>
