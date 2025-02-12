070411e5f193f2cf7fa3a860a3b79ecfcb511103
Rek <rekkisomo@pm.me>
2025-02-12 15:20:36 +1100

    ui: Speed up pack loading a ton

--- app/pack/[slug]/page.tsx
+++ app/pack/[slug]/page.tsx
@@ -1,10 +1,8 @@
 'use client'
 import FeedList from '@/components/shared/feed/list'
-import { LoadingCircle } from '@/components/shared/icons'
-import { vg } from '@/lib/api'
-import { useUIStore, useUserAccountStore } from '@/lib/states'
+import { useResourceStore, useUIStore, useUserAccountStore } from '@/lib/states'
 import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 const GuestLanding = dynamic(() => import('@/components/home/guestlanding'))
 const PackHeader = dynamic(() => import('@/components/shared/pack/header'))
@@ -12,53 +10,23 @@ const PackHeader = dynamic(() => import('@/components/shared/pack/header'))
 export default function Home({ params }: { params: { slug: string } }) {
     const { user } = useUserAccountStore()
     const { setHidden } = useUIStore()
+    const { currentResource } = useResourceStore()
     // const Lottie = memo(dynamic(() => import('lottie-react'), { ssr: false, suspense: true }))
-    const [showModal, setShowModal] = useState<boolean>(false)
-    const [loading, setLoading] = useState<boolean>(true)
-    const [, setError] = useState<string | null>(null)
-    const [pack, setPack] = useState<any>()
 
     useEffect(() => {
         if (!user) setHidden(true)
     }, [user])
 
-    useEffect(() => {
-        vg.pack({ id: params.slug })
-            .get()
-            .then(({ data }) => {
-                if (!data || data?.message) {
-                    setLoading(false)
-                    return setError('failed')
-                }
-                setLoading(false)
-                setPack(data)
-            })
-            .catch((e) => {
-                setError(e)
-                setLoading(false)
-            })
-    }, [])
-
     return (
         <>
             {!user && <GuestLanding />}
 
-            {loading && (
-                <div className="flex h-full items-center justify-center">
-                    <LoadingCircle />
+            {currentResource && currentResource?.slug !== 'universe' && <PackHeader pack={currentResource} />}
+
+            {user && !user.anonUser && (
+                <div className="p-8">
+                    <FeedList packID={currentResource.id} />
                 </div>
-            )}
-
-            {!loading && (
-                <>
-                    {pack && pack?.slug !== 'universe' && <PackHeader pack={pack} />}
-
-                    {user && !user.anonUser && (
-                        <div className="p-8">
-                            <FeedList packID={pack.id} />
-                        </div>
-                    )}
-                </>
             )}
         </>
     )
