daec91f21682d4b374dfca5827c91ea46dfbfbd9
Rek <rekkisomo@pm.me>
2025-02-17 17:13:42 +1100

    ui: Links coloured again

--- components/shared/link.tsx
+++ components/shared/link.tsx
@@ -1,11 +1,12 @@
 import * as Headless from '@headlessui/react'
-import NextLink, { type LinkProps } from 'next/link'
-import { ComponentPropsWithoutRef, ForwardedRef, forwardRef } from 'react'
+import NextLink, {type LinkProps} from 'next/link'
+import {ComponentPropsWithoutRef, ForwardedRef, forwardRef} from 'react'
+import clsx from 'clsx'
 
 const Link = forwardRef(function Link(props: LinkProps & ComponentPropsWithoutRef<'a'>, ref: ForwardedRef<HTMLAnchorElement>) {
     return (
         <Headless.DataInteractive>
-            <NextLink {...props} ref={ref} />
+            <NextLink {...props} className={clsx('!text-primary', props.className)} ref={ref} />
         </Headless.DataInteractive>
     )
 })
