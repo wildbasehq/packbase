abccf88c57709198c437ab4ebff71740b1dcf16d
Rek <rekkisomo@pm.me>
2025-02-17 15:44:09 +1100

    ui: Fix tailwind's failure to combine inputs

--- components/shared/input/text.tsx
+++ components/shared/input/text.tsx
@@ -28,9 +28,9 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
                 <div className="flex">
                     <div
                         className={cn(
-                            combined ? 'rounded-0 border-0 shadow-none' : 'rounded shadow-xs ring-1 ring-inset ring-neutral-300 dark:ring-white/10',
+                            combined ? '!rounded-0 !border-0 shadow-none' : 'rounded shadow-xs ring-1 ring-inset ring-neutral-300 dark:ring-white/10',
                             className,
-                            'bg-default rounded flex w-full focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600! sm:max-w-md',
+                            'bg-default flex w-full focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600! sm:max-w-md',
                         )}
                     >
                         {suffix && <span className="-mr-2.5 flex select-none items-center pl-3 text-neutral-500 sm:text-sm">{suffix}</span>}
