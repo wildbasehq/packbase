17f18d6c2987048e29cba927aec12fac9912fd59
Rekkisomo <im@rek.lol>
2025-04-27 18:24:24 +1000

    💄 Sidebar hover items unified

--- components/shared/sidebar.tsx
+++ components/shared/sidebar.tsx
@@ -91,21 +91,21 @@ export const SidebarItem = forwardRef(function SidebarItem(
         // Base
         'flex w-full items-center gap-3 rounded-lg px-2 py-2.5 text-left text-base/6 font-medium text-zinc-950 sm:py-2 sm:text-sm/5',
         // Leading icon/icon-only
-        '*:data-[slot=icon]:size-6 *:data-[slot=icon]:shrink-0 *:data-[slot=icon]:fill-zinc-500 sm:*:data-[slot=icon]:size-5',
+        '*:data-[slot=icon]:size-6 *:data-[slot=icon]:shrink-0 *:data-[slot=icon]:fill-n-5 sm:*:data-[slot=icon]:size-5',
         // Trailing icon (down chevron or similar)
         '*:last:data-[slot=icon]:ml-auto *:last:data-[slot=icon]:size-5 sm:*:last:data-[slot=icon]:size-4',
         // Avatar
         '*:data-[slot=avatar]:-m-0.5 *:data-[slot=avatar]:size-7 *:data-[slot=avatar]:[--ring-opacity:10%] sm:*:data-[slot=avatar]:size-6',
         // Hover
-        'data-hover:bg-zinc-950/5 data-hover:*:data-[slot=icon]:fill-zinc-950',
+        'data-hover:bg-n-2/25 data-hover:*:data-[slot=icon]:fill-n-9 data-hover:ring-2 ring-default transition-all',
         // Active
-        'data-active:bg-zinc-950/5 data-active:*:data-[slot=icon]:fill-zinc-950',
+        'data-active:bg-n-2/25 data-active:*:data-[slot=icon]:fill-n-9',
         // Current
-        'data-current:*:data-[slot=icon]:fill-zinc-950',
+        'data-current:*:data-[slot=icon]:fill-n-9',
         // Dark mode
-        'dark:text-white dark:*:data-[slot=icon]:fill-zinc-400',
-        'dark:data-hover:bg-white/5 dark:data-hover:*:data-[slot=icon]:fill-white',
-        'dark:data-active:bg-white/5 dark:data-active:*:data-[slot=icon]:fill-white',
+        'dark:text-white dark:*:data-[slot=icon]:fill-n-4',
+        'dark:data-hover:bg-n-6/50 dark:data-hover:*:data-[slot=icon]:fill-white',
+        'dark:data-active:bg-n-6/50 dark:data-active:*:data-[slot=icon]:fill-white',
         'dark:data-current:*:data-[slot=icon]:fill-white'
     )
 
