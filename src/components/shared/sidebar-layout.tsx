d7286520d1d7745f021ca985c670271eb4963f77
Rek <im@rek.lol>
2025-05-15 06:06:56 +1000

    🚧 Basis for Your Stuff, silently add the UI.

--- src/components/shared/sidebar-layout.tsx
+++ src/components/shared/sidebar-layout.tsx
@@ -4,8 +4,7 @@ import { useUIStore, useUserAccountStore } from '@/lib/state'
 import { NavbarItem } from '@/components/layout'
 import { useLocation } from 'wouter'
 import { motion } from 'framer-motion'
-import { Heading, Text } from '@/components/shared/text.tsx'
-import FolderCollectionIcon from '@/components/icons/folder-collection.tsx'
+import YourStuffPage from '@/pages/stuff/page.tsx'
 
 function OpenMenuIcon() {
     return (
@@ -81,24 +80,7 @@ export function SidebarLayout({
             {/* Content */}
             <div className="flex flex-col flex-1 relative">
                 <div className="hidden lg:block">{navbar}</div>
-                {isSettingsPage && (
-                    <div className="absolute flex justify-center items-center h-[calc(100vh-14rem)] lg:w-[calc(100%-24rem)] top-14 lg:ml-96 overflow-hidden grow">
-                        <div className="h-full flex items-center justify-center">
-                            <div className="w-full max-w-2xl">
-                                <div className="flex flex-col items-center justify-center">
-                                    <div className="flex flex-col items-center justify-center max-w-xs text-center mb-4">
-                                        <FolderCollectionIcon className="h-24 mb-6" />
-                                        <Heading className="mb-2">You have no stuff yet</Heading>
-                                        <Text alt>
-                                            Your "stuff" is collections of content you've saved across Packbase or copy-and-pasted into
-                                            here, and will always have quick access to.
-                                        </Text>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
+                {isSettingsPage && <YourStuffPage />}
                 <motion.main
                     // key={isSettingsPage ? 'settings' : 'default'}
                     className={`pb-2 lg:min-w-0 lg:pr-2 ${!hidden && user ? 'lg:pl-96' : 'lg:pl-2'} ${isSettingsPage ? '!z-10' : 'z-0'}`}
