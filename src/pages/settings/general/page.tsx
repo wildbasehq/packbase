3f2eaae424ce9f56df6a48f5cf13f6a932584d96
Rek <im@rek.lol>
2025-05-30 18:06:22 +1000

    🐛 About Me Saving

--- src/pages/settings/general/page.tsx
+++ src/pages/settings/general/page.tsx
@@ -1,14 +1,48 @@
 import { Field, Label, Textarea } from '@/components/shared'
 import React, { useEffect } from 'react'
-import { useUserAccountStore } from '@/lib'
+import { useUserAccountStore, vg } from '@/lib'
+import { toast } from 'sonner'
+import { Button } from '@/components/shared/experimental-button-rework'
 
 const ProfileSettings: React.FC = () => {
     const { user } = useUserAccountStore()
     const bioRef = React.useMemo(() => React.createRef<HTMLTextAreaElement>(), [])
 
+    const [submitting, setSubmitting] = React.useState<boolean>(false)
+
     useEffect(() => {
         if (bioRef.current) bioRef.current.value = user?.about?.bio || ''
     }, [])
+
+    function saveProfile(e: { preventDefault: () => void }) {
+        e.preventDefault()
+        if (submitting) return
+        setSubmitting(true)
+
+        vg.user.me
+            .post({
+                about: {
+                    bio: bioRef.current?.value || undefined,
+                },
+            })
+            .then(({ data, error }) => {
+                if (data && !error) {
+                    toast.success('Profile updated')
+                    window.location.reload()
+                } else {
+                    setSubmitting(false)
+                    toast.error(
+                        "Couldn't save: " +
+                            (error.value ? `${error.status}: ${error.value.summary || error.value.error}` : 'Something went wrong')
+                    )
+                }
+            })
+            .catch(err => {
+                setSubmitting(false)
+                toast.error("Couldn't save: " + (err.message ? `${err.cause}: ${err}` : 'Something went wrong'))
+            })
+    }
+
     return (
         <form>
             <div className="border-b pb-4 mb-4 border-n-5/10">
@@ -19,6 +53,12 @@ const ProfileSettings: React.FC = () => {
                 <Label>About Me</Label>
                 <Textarea ref={bioRef} name="bio" />
             </Field>
+
+            <div className="mt-4">
+                <Button type="submit" className="btn btn-primary" onClick={saveProfile} disabled={submitting}>
+                    {submitting ? 'Saving...' : 'Save'}
+                </Button>
+            </div>
         </form>
     )
 }
