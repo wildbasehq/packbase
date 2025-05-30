4e6a1136f0acd58993cc9cd1fa929102c1fd3a6b
Rek <im@rek.lol>
2025-05-30 18:17:13 +1000

    🐛 Add your profile link to dropdown

--- src/components/layout/user-dropdown.tsx
+++ src/components/layout/user-dropdown.tsx
@@ -3,11 +3,18 @@ import { SignedIn, UserButton } from '@clerk/clerk-react'
 import { EnvelopeOpenIcon, UserIcon } from '@heroicons/react/24/solid'
 import ProfileSettings from '@/pages/settings/general/page.tsx'
 import InviteSettings from '@/pages/settings/invite/page.tsx'
+import { useLocation } from 'wouter'
+import { useUserAccountStore } from '@/lib'
 
 export default function UserDropdown() {
+    const { user } = useUserAccountStore()
+    const [location, setLocation] = useLocation()
     return (
         <SignedIn>
             <UserButton>
+                <UserButton.MenuItems>
+                    <UserButton.Action label="Your Profile" labelIcon={<UserIcon />} onClick={() => setLocation(`/@${user.username}`)} />
+                </UserButton.MenuItems>
                 <UserButton.UserProfilePage label="Profile" url="/profile" labelIcon={<UserIcon />}>
                     <ProfileSettings />
                 </UserButton.UserProfilePage>
