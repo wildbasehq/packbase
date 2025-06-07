024404aaed412e9a125227af64663d9ce05f19bf
Rek <im@rek.lol>
2025-06-07 20:18:24 +1000

    🐛 display_name changing

--- src/pages/settings/general/page.tsx
+++ src/pages/settings/general/page.tsx
@@ -1,4 +1,8 @@
-import { Field, Label, Textarea } from '@/components/shared'
+/*
+ * Copyright (c) Wildbase 2025. All rights and ownership reserved. Not for distribution.
+ */
+
+import { Field, Input, Label, Textarea } from '@/components/shared'
 import React, { useEffect } from 'react'
 import { useUserAccountStore, vg } from '@/lib'
 import { toast } from 'sonner'
@@ -7,11 +11,13 @@ import { Button } from '@/components/shared/experimental-button-rework'
 const ProfileSettings: React.FC = () => {
     const { user } = useUserAccountStore()
     const bioRef = React.useMemo(() => React.createRef<HTMLTextAreaElement>(), [])
+    const displayNameRef = React.useMemo(() => React.createRef<HTMLInputElement>(), [])
 
     const [submitting, setSubmitting] = React.useState<boolean>(false)
 
     useEffect(() => {
         if (bioRef.current) bioRef.current.value = user?.about?.bio || ''
+        if (displayNameRef.current) displayNameRef.current.value = user?.display_name || ''
     }, [])
 
     function saveProfile(e: { preventDefault: () => void }) {
@@ -21,6 +27,7 @@ const ProfileSettings: React.FC = () => {
 
         vg.user.me
             .post({
+                display_name: displayNameRef.current?.value || user?.display_name,
                 about: {
                     bio: bioRef.current?.value || undefined,
                 },
@@ -48,6 +55,15 @@ const ProfileSettings: React.FC = () => {
             <div className="border-b pb-4 mb-4 border-n-5/10">
                 <h1 className="font-bold text-[17px]">Profile Settings</h1>
             </div>
+
+            <div className="mb-4">
+                <p className="text-sm text-muted-foreground">Username can be changed in your "Account" tab.</p>
+            </div>
+
+            <Field>
+                <Label>Display Name</Label>
+                <Input ref={displayNameRef} name="display_name" />
+            </Field>
 
             <Field>
                 <Label>About Me</Label>
