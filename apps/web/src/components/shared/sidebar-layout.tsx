4166552656468311c04c206190841702b6c2e5cb
Rek <im@rek.lol>
2025-08-23 13:54:38 +1000

    Outage notice banner

--- apps/web/src/components/shared/sidebar-layout.tsx
+++ apps/web/src/components/shared/sidebar-layout.tsx
@@ -25,11 +25,15 @@ import {
     SidebarSection,
     SidebarSpacer,
 } from '@/src/components'
-import { QuestionMarkCircleIcon, SparklesIcon } from '@heroicons/react/20/solid'
+import { QuestionMarkCircleIcon, ShieldExclamationIcon, SparklesIcon } from '@heroicons/react/20/solid'
 import { FaceSmileIcon } from '@heroicons/react/16/solid'
 import { SiDiscord } from 'react-icons/si'
 import WildbaseAsteriskIcon from '@/components/icons/wildbase-asterisk.tsx'
 import { useSession } from '@clerk/clerk-react'
+import { XIcon } from 'lucide-react'
+import { cn } from '@/lib'
+import { Text } from '@/components/shared/text.tsx'
+import Markdown from '@/components/shared/markdown.tsx'
 
 function OpenMenuIcon() {
     return (
@@ -73,112 +77,110 @@ function MobileSidebar({ open, close, children }: React.PropsWithChildren<{ open
 
 export function SidebarLayout({ children }: React.PropsWithChildren) {
     let [showSidebar, setShowSidebar] = useState(false)
-    // const [showVGSNotice, setShowVGSNotice] = useState(true)
+    const [showVGSNotice, setShowVGSNotice] = useState(true)
     const { isSignedIn } = useSession()
     const { sidebarContent } = useSidebar()
     // const [location] = useLocation()
     // const isStuffPage = location === '/stuff'
 
     return (
-        <div className="relative flex w-full bg-white isolate min-h-svh max-lg:flex-col lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950">
-            {/* Sidebar on desktop */}
-            {isSignedIn && (
-                <div className="fixed inset-y-0 left-0 z-20 max-lg:hidden flex h-full">
-                    <PackSwitcher />
+        <div className="w-full bg-white isolate min-h-svh max-lg:flex-col lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950">
+            {showVGSNotice && (
+                <div className="top-0 z-50 mx-2 my-2">
+                    <div className="bg-amber-50 border-2 ring-2 ring-amber-500/10 border-amber-500 p-4 rounded-md shadow-sm dark:bg-amber-900/20 dark:border-amber-600">
+                        <div className="flex items-center">
+                            <div className="flex-shrink-0">
+                                <ShieldExclamationIcon className="h-5 w-5 text-amber-500 dark:text-amber-400" aria-hidden="true" />
+                            </div>
+                            <div className="ml-3 flex-1">
+                                <Markdown componentClassName="text-sm !text-amber-700 dark:!text-amber-200">
+                                    **Notice:** Some images are not loading due to an issue with our storage provider - This only affects
+                                    content older than 30 days **and** hosted on Packbase. Content hosted yourself is not affected. We're
+                                    working on a fix and will remove this notice once it's resolved.
+                                </Markdown>
+                            </div>
+                            <div>
+                                <button
+                                    type="button"
+                                    className="inline-flex rounded p-1.5 text-amber-500 hover:bg-amber-100 dark:hover:bg-amber-800 focus:outline-none focus:ring-2 focus:ring-amber-500"
+                                    onClick={() => setShowVGSNotice(false)}
+                                >
+                                    <span className="sr-only">Dismiss</span>
+                                    <XIcon className="h-5 w-5" aria-hidden="true" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )}
 
-            {/* Sidebar on mobile */}
-            <MobileSidebar open={showSidebar} close={() => setShowSidebar(false)}>
-                <SidebarContentContainer>{sidebarContent}</SidebarContentContainer>
-            </MobileSidebar>
-
-            {/* Navbar on mobile */}
-            <header className="flex items-center px-4 lg:hidden z-10">
-                <div className="py-2.5">
-                    <NavbarItem onClick={() => setShowSidebar(true)} aria-label="Open navigation">
-                        <OpenMenuIcon />
-                    </NavbarItem>
-                </div>
-            </header>
-
-            {/* Content */}
-            <div className="flex flex-col flex-1 relative">
-                {/*<div className="hidden lg:block">{navbar}</div>*/}
-                {/*{isStuffPage && <YourStuffPage />}*/}
-                <main className={`py-2 lg:min-w-0 lg:pr-2 ${isSignedIn ? 'lg:pl-18' : 'lg:pl-2'}`}>
-                    {/*<motion.main*/}
-                    {/*    // key={isStuffPage ? 'settings' : 'default'}*/}
-                    {/*    className={`pb-2 lg:min-w-0 lg:pr-2 ${user ? 'lg:pl-96' : 'lg:pl-2'} ${isStuffPage ? '!z-10' : 'z-0'}`}*/}
-                    {/*    initial={false}*/}
-                    {/*    animate="animate"*/}
-                    {/*    transition={{*/}
-                    {/*        type: 'spring',*/}
-                    {/*        stiffness: 600,*/}
-                    {/*        damping: 40,*/}
-                    {/*        mass: 1,*/}
-                    {/*        bounce: 0,*/}
-                    {/*    }}*/}
-                    {/*    custom={{ isStuffPage }}*/}
-                    {/*    variants={{*/}
-                    {/*        initial: {*/}
-                    {/*            y: 0,*/}
-                    {/*            filter: 'blur(0px)',*/}
-                    {/*            opacity: 1,*/}
-                    {/*        },*/}
-                    {/*        animate: ({ isStuffPage }) => ({*/}
-                    {/*            y: isStuffPage ? '80%' : 0,*/}
-                    {/*            filter: isStuffPage ? 'blur(2px)' : 'blur(0px)',*/}
-                    {/*            // opacity: isSettingsPage ? 0.5 : 1,*/}
-                    {/*        }),*/}
-                    {/*    }}*/}
-                    {/*>*/}
-
-                    {/*{showVGSNotice && (*/}
-                    {/*    <div className="fixed top-0 z-50 mx-2 my-2">*/}
-                    {/*        <div className="bg-amber-50 border-2 ring-2 ring-amber-500/10 border-amber-500 p-4 rounded-md shadow-sm dark:bg-amber-900/20 dark:border-amber-600">*/}
-                    {/*            <div className="flex items-center">*/}
-                    {/*                <div className="flex-shrink-0">*/}
-                    {/*                    <ShieldExclamationIcon className="h-5 w-5 text-amber-500 dark:text-amber-400" aria-hidden="true" />*/}
-                    {/*                </div>*/}
-                    {/*                <div className="ml-3 flex-1">*/}
-                    {/*                    <p className="text-sm text-amber-700 dark:text-amber-200">*/}
-                    {/*                        <span className="font-medium">Notice:</span> We are currently experiencing higher than normal*/}
-                    {/*                        latency due to an issue on the VGS. Some services may be temporarily slower than usual. Please*/}
-                    {/*                        check back later, or go to{' '}*/}
-                    {/*                        <Link href="https://vgs.packbase.app/stats" target="_blank">*/}
-                    {/*                            VGS Health*/}
-                    {/*                        </Link>{' '}*/}
-                    {/*                        for more information.*/}
-                    {/*                    </p>*/}
-                    {/*                </div>*/}
-                    {/*                <div>*/}
-                    {/*                    <button*/}
-                    {/*                        type="button"*/}
-                    {/*                        className="inline-flex rounded p-1.5 text-amber-500 hover:bg-amber-100 dark:hover:bg-amber-800 focus:outline-none focus:ring-2 focus:ring-amber-500"*/}
-                    {/*                        onClick={() => setShowVGSNotice(false)}*/}
-                    {/*                    >*/}
-                    {/*                        <span className="sr-only">Dismiss</span>*/}
-                    {/*                        <XIcon className="h-5 w-5" aria-hidden="true" />*/}
-                    {/*                    </button>*/}
-                    {/*                </div>*/}
-                    {/*            </div>*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*)}*/}
-
-                    <div className="relative h-[calc(100vh-1rem)] flex overflow-hidden grow lg:rounded-lg lg:bg-white lg:ring-1 lg:shadow-xs lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10">
-                        {isSignedIn && (
-                            <div className="top-0 backdrop-blur border-r h-full flex z-10">
-                                <SidebarContentContainer>{sidebarContent}</SidebarContentContainer>
-                            </div>
-                        )}
-                        <div className={cx('mx-auto w-full overflow-hidden h-full')}>{children}</div>
+            <div className="relative flex">
+                {/* Sidebar on desktop */}
+                {isSignedIn && (
+                    <div className={cn('fixed inset-y-0 left-0 z-20 max-lg:hidden flex h-full', showVGSNotice && 'top-20')}>
+                        <PackSwitcher />
                     </div>
-                    {/*</motion.main>*/}
-                </main>
+                )}
+
+                {/* Sidebar on mobile */}
+                <MobileSidebar open={showSidebar} close={() => setShowSidebar(false)}>
+                    <SidebarContentContainer>{sidebarContent}</SidebarContentContainer>
+                </MobileSidebar>
+
+                {/* Navbar on mobile */}
+                <header className="flex items-center px-4 lg:hidden z-10">
+                    <div className="py-2.5">
+                        <NavbarItem onClick={() => setShowSidebar(true)} aria-label="Open navigation">
+                            <OpenMenuIcon />
+                        </NavbarItem>
+                    </div>
+                </header>
+
+                {/* Content */}
+                <div className="flex flex-col flex-1 relative">
+                    {/*<div className="hidden lg:block">{navbar}</div>*/}
+                    {/*{isStuffPage && <YourStuffPage />}*/}
+                    <main className={`py-2 lg:min-w-0 lg:pr-2 ${isSignedIn ? 'lg:pl-18' : 'lg:pl-2'}`}>
+                        {/*<motion.main*/}
+                        {/*    // key={isStuffPage ? 'settings' : 'default'}*/}
+                        {/*    className={`pb-2 lg:min-w-0 lg:pr-2 ${user ? 'lg:pl-96' : 'lg:pl-2'} ${isStuffPage ? '!z-10' : 'z-0'}`}*/}
+                        {/*    initial={false}*/}
+                        {/*    animate="animate"*/}
+                        {/*    transition={{*/}
+                        {/*        type: 'spring',*/}
+                        {/*        stiffness: 600,*/}
+                        {/*        damping: 40,*/}
+                        {/*        mass: 1,*/}
+                        {/*        bounce: 0,*/}
+                        {/*    }}*/}
+                        {/*    custom={{ isStuffPage }}*/}
+                        {/*    variants={{*/}
+                        {/*        initial: {*/}
+                        {/*            y: 0,*/}
+                        {/*            filter: 'blur(0px)',*/}
+                        {/*            opacity: 1,*/}
+                        {/*        },*/}
+                        {/*        animate: ({ isStuffPage }) => ({*/}
+                        {/*            y: isStuffPage ? '80%' : 0,*/}
+                        {/*            filter: isStuffPage ? 'blur(2px)' : 'blur(0px)',*/}
+                        {/*            // opacity: isSettingsPage ? 0.5 : 1,*/}
+                        {/*        }),*/}
+                        {/*    }}*/}
+                        {/*>*/}
+
+                        <div className="relative h-[calc(100vh-1rem)] flex overflow-hidden grow lg:rounded-lg lg:bg-white lg:ring-1 lg:shadow-xs lg:ring-zinc-950/5 dark:lg:bg-zinc-900 dark:lg:ring-white/10">
+                            {isSignedIn && (
+                                <div className="top-0 backdrop-blur border-r h-full flex z-10">
+                                    <SidebarContentContainer>{sidebarContent}</SidebarContentContainer>
+                                </div>
+                            )}
+                            <div className={cx('mx-auto w-full overflow-hidden h-full')}>{children}</div>
+                        </div>
+                        {/*</motion.main>*/}
+                    </main>
+                </div>
+                {isSignedIn && <UserSidebar />}
             </div>
-            {isSignedIn && <UserSidebar />}
         </div>
     )
 }
