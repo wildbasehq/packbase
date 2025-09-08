29689026e0303ea055b25291e66daa4a1c31fbbb
Rek <im@rek.lol>
2025-09-09 02:10:12 +1000

    oops

--- apps/web/src/pages/pack/[slug]/layout.tsx
+++ apps/web/src/pages/pack/[slug]/layout.tsx
@@ -164,7 +164,7 @@ export default function PackLayout({ children }: { children: React.ReactNode }) 
         )
 
     return (
-        <SafeFrame>
+        <SafeFrame className="w-full h-full">
             <SidebarPortal>
                 <PackChannels />
             </SidebarPortal>
