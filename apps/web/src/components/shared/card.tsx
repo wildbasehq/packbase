5b6084be1b24b47a3375db3185e0bd627e99ea52
Rek <im@rek.lol>
2025-09-09 02:33:33 +1000

    grrrr

--- apps/web/src/components/shared/card.tsx
+++ apps/web/src/components/shared/card.tsx
@@ -3,7 +3,10 @@ import { cn } from '@/lib/utils'
 export default function Card({ children, className, ...props }: { children: React.ReactNode; className?: string; [key: string]: any }) {
     return (
         <div
-            className={cn(className, 'flex h-fit w-full flex-col rounded border bg-sidebar px-2 py-3 sm:max-w-md sm:px-3 sm:py-4')}
+            className={cn(
+                className,
+                'flex h-fit w-full flex-col rounded border bg-white dark:bg-n-8 px-2 py-3 sm:max-w-md sm:px-3 sm:py-4'
+            )}
             {...props}
         >
             {children}
