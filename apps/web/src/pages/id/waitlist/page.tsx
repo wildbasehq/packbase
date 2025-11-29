8f90b818646f9df80e98b4e3c264e3ec9d300262
Rekkisomo Harley <im@rek.lol>
2025-11-29 21:16:27 +1100

    feat: Add new alert for registration status and adjust text formatting in waitlist page

--- apps/web/src/pages/id/waitlist/page.tsx
+++ apps/web/src/pages/id/waitlist/page.tsx
@@ -3,9 +3,9 @@
  */
 
 import Link from '@/components/shared/link'
-import { Waitlist } from '@clerk/clerk-react'
-import { Text } from '@/components/shared/text.tsx'
-import { Alert, AlertDescription, AlertTitle } from '@/src/components'
+import {Waitlist} from '@clerk/clerk-react'
+import {Text} from '@/components/shared/text.tsx'
+import {Alert, AlertDescription, AlertTitle} from '@/src/components'
 
 export default function IDWaitlist() {
     return (
@@ -13,13 +13,23 @@ export default function IDWaitlist() {
             <Alert variant="destructive" className="shadow-xs mb-4">
                 <AlertTitle>Age Requirement Changes</AlertTitle>
                 <AlertDescription>
-                    On 10th of December 2025, we will be changing the age requirement to 18+, but content you can and cannot upload will be
+                    On 10th of December 2025, we will be changing the age requirement to 18+, but content you can and
+                    cannot upload will be
                     the same.
                 </AlertDescription>
             </Alert>
 
+            <Alert variant="destructive" className="shadow-xs mb-4">
+                <AlertTitle>Registration is closed</AlertTitle>
+                <AlertDescription>
+                    We open and close registration based on traffic. Those on the waitlist will be first in line when
+                    registration opens. If the waitlist is too large, registration will be closed for even longer, so
+                    we encourage you to ask a friend to invite you instead.
+                </AlertDescription>
+            </Alert>
+
             <div className="shadow-xs ring-1 ring-n-5/10 rounded overflow-clip">
-                <Waitlist signInUrl="/id/login" />
+                <Waitlist signInUrl="/id/login"/>
             </div>
 
             <Text>
