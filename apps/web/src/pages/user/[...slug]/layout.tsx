54f5e082f2e71c23d527093bf078176e02c74f72
Rekkisomo <im@rek.lol>
2025-12-19 20:02:19 +1100

    feat(user-layout): enhance R18 verification logic to include current user check

--- apps/web/src/pages/user/[...slug]/layout.tsx
+++ apps/web/src/pages/user/[...slug]/layout.tsx
@@ -7,17 +7,18 @@ import {SidebarPortal} from '@/lib/context/sidebar-context'
 import UserFolders from '@/components/layout/user-folders'
 import {useParams} from "wouter";
 import {useContentFrame} from "@/src/components";
-import {isVisible} from "@/lib";
+import {isVisible, useUserAccountStore} from "@/lib";
 import PackbaseInstance from "@/lib/workers/global-event-emit.ts";
 
 export default function UserLayout({children}: { children: ReactNode }) {
     const {slug} = useParams<{ slug: string }>()
+    const {user: currentUser} = useUserAccountStore()
 
     const {data: user, isLoading} = useContentFrame('get', `user.${slug}`)
 
     useEffect(() => {
         // if user.is_r18 is true, ask for verification
-        if (user?.is_r18) {
+        if (user?.is_r18 && currentUser.id !== user.id) {
             PackbaseInstance.emit('request-r18-confirmation', {
                 r18_tags: user.r18_tags || []
             })
