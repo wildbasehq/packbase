c15366b26f10ce5f9d4a1b096db351daba538f5d
Rekkisomo <im@rek.lol>
2026-01-16 03:34:39 +1100

    why
    #private

--- apps/server/src/db/prisma.ts
+++ apps/server/src/db/prisma.ts
@@ -1,6 +1,6 @@
 import {PrismaPg} from '@prisma/adapter-pg'
+import {PrismaClient} from '@prisma/client'
 import Debug from 'debug'
-import {PrismaClient} from '../../prisma/generated/client'
 
 const log = {
     info: Debug('vg:prisma'),
