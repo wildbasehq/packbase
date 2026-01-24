d8001a32a84da2da3a9a6b6e157369a64f2eb6c9
Rekkisomo <im@rek.lol>
2026-01-25 01:07:32 +1100

    what
    #private

--- apps/server/src/routes/leaderboard/profiles.ts
+++ apps/server/src/routes/leaderboard/profiles.ts
@@ -222,6 +222,7 @@ export default (app: YapockType) =>
                             profile: UserProfile,
                         }),
                     ),
+                    update_in: t.String(),
                 }),
             },
         },
