d73089524a5c80a2a5de043fac139dc3385a48bf
Rekkisomo <im@rek.lol>
2026-02-03 18:00:03 +1100

    refactor: Remove useless clerk test, it runs in SDK

--- apps/server/src/db/auth.ts
+++ apps/server/src/db/auth.ts
@@ -1,10 +1,4 @@
 import {createClerkClient} from '@clerk/backend'
-import Debug from 'debug'
-
-const log = {
-    info: Debug('vg:clerk'),
-    error: Debug('vg:clerk:error'),
-}
 
 const clerkClient = createClerkClient({
     publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
@@ -12,22 +6,3 @@ const clerkClient = createClerkClient({
 })
 
 export default clerkClient
-
-// Test the client connection
-async function clientTest() {
-    const clientTest = await clerkClient.users.getUserList({limit: 1})
-
-    if (!!clientTest.data) {
-        log.info('Clerk client connected')
-    } else {
-        log.error('Clerk client failed to connect')
-        process.exit(1)
-    }
-}
-
-try {
-    await clientTest()
-} catch (error) {
-    log.error('Clerk client failed to connect', error)
-    process.exit(1)
-}
