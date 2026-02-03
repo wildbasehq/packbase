b1168b50369f1de60d8a837c9f7c32b6f7d603bd
Rekkisomo <im@rek.lol>
2026-02-03 21:01:10 +1100

    fix: Loading circle using old spinner

--- apps/web/src/components/icons/loading-circle.tsx
+++ apps/web/src/components/icons/loading-circle.tsx
@@ -1,6 +1,8 @@
 import {cn} from '@/lib'
-import ProcessWorkingSymbol from '@/src/images/symbolic/process-working.symbolic.png'
+import {LoadingSpinner} from '@/src/components'
 
 export default function LoadingCircle({...props}) {
-    return <img className={cn('h-5 w-5 animate-spin dark:invert', props.className)} src={ProcessWorkingSymbol} alt="Process working spinner"/>
+    return <div className={cn('h-5 w-5', props.className)}>
+        <LoadingSpinner/>
+    </div>
 }
